7642297910a7ef904efc1be7758534431704cf88
Jacob Scott <jacob.scott@gmail.com>
2025-02-08 16:20:23 -0800

    format misc

--- interview/code.py
+++ interview/code.py
@@ -30,6 +30,13 @@ def times_two(x: int) -> int:
     return x * 2
 
 
-@pytest.mark.parametrize(("input", "expected"), ((0, 0), (1, 2), (-2, -4)))
+@pytest.mark.parametrize(
+    ("input", "expected"),
+    (  # format
+        (0, 0),
+        (1, 2),
+        (-2, -4),
+    ),
+)
 def test_hello_world(input, expected):
     assert times_two(input) == expected
