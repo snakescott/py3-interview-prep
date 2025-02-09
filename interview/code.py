70b42e563d8f8ff6ae0b1505fe93cdb6b8afed5a
Jacob Scott <jacob.scott@gmail.com>
2025-02-08 16:54:51 -0800

    mypy, more_itertools, AOC 2024 d2

--- interview/code.py
+++ interview/code.py
@@ -1,4 +1,42 @@
+import more_itertools
 import pytest
+
+
+# AOC 2024 day 2
+def is_safe(l: list[int]) -> bool:
+    if len(l) <= 1:
+        return True
+
+    sentinel = max(l) - min(l)
+    low = sentinel
+    high = -1 * sentinel
+    for w in more_itertools.sliding_window(l, 2):
+        diff = w[0] - w[1]
+        low = min(low, diff)
+        high = max(high, diff)
+
+    # Handles cases with no difference as well as different signed diffs
+    if low * high <= 0:
+        return False
+
+    if abs(low) > 3 or abs(high) > 3:
+        return False
+
+    return True
+
+
+@pytest.mark.parametrize(
+    ("input", "expected"),
+    (  # format
+        ([7, 6, 4, 2, 1], True),
+        ([], True),
+        ([1], True),
+        ([1, 2, 7, 8, 9], False),
+        ([1, 3, 2, 4, 5], False),
+    ),
+)
+def test_is_safe(input: list[int], expected: bool) -> None:
+    assert is_safe(input) == expected
 
 
 # AOC 2024 day 1
@@ -38,5 +76,5 @@ def times_two(x: int) -> int:
         (-2, -4),
     ),
 )
-def test_hello_world(input, expected):
+def test_hello_world(input: int, expected: int) -> None:
     assert times_two(input) == expected
